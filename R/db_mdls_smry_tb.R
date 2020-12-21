b8370f533ea21fbbce80afe56c2e15a6d731958e
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-12-22 08:22:35 +1100

    added vignettes

---
+++ R/db_mdls_smry_tb.R
@@ -0,0 +1,14 @@
+#' Summary coefficients for map2aqol transfer to utility models
+#' 
+#' A summary of the models included in the map2aqol package that can be used to predict adolescent AQoL6D. Note this summary is a placeholder as all models have been estimated from synthetic data.
+#' 
+#' A tibble
+#' 
+#' \describe{
+#'   \item{Model}{NO MATCH}
+#'   \item{Parameter}{NO MATCH}
+#'   \item{Estimate}{NO MATCH}
+#'   \item{SE}{NO MATCH}
+#'   \item{CI_95}{NO MATCH}
+#' }
+"mdls_smry_tb"
