b27a132d4f008b76ea73a7e34bfead461a03c2eb
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-02-18 16:35:09 +1100

    updated for deps

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -183,6 +183,7 @@ pkg_dss_tb <- tibble::tibble(mdl_nms_chr = mdls_smry_tb$Model %>% unique()) %>%
 ## 8. Document functions.
 usethis::use_build_ignore("initial_setup.R")
 usethis::use_package("knitrBootstrap")
+usethis::use_package("truncnorm")
 usethis::use_dev_package("ready4show")
 usethis::use_dev_package("ready4use")
 usethis::use_dev_package("TTU")
