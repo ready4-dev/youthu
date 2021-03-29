4d2036ee32a560c905e7bffa196fe01b668b5a88
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-03-29 17:03:18 +1100

    Trivial edit to trigger new build on Ubuntu

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -138,3 +138,4 @@ ready4fun::write_and_doc_fn_fls(fns_dmt_tb,
                                 path_to_dvpr_dmt_dir_1L_chr = "../../../../../Documentation/Code/Developer",
                                 path_to_user_dmt_dir_1L_chr = "../../../../../Documentation/Code/User")
 devtools::build_vignettes()
+#
