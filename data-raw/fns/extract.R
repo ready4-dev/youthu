5641ebfbc524b7c645d8b515becabeb27ad40b64
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-04-15 07:58:06 +1000

    updated dpcy

--- data-raw/fns/extract.R
+++
@@ -1,7 +0,0 @@
-extract_guide_box_lgd <- function (plot_plt)
-{
-    tmp <- ggplot2::ggplot_gtable(ggplot2::ggplot_build(plot_plt))
-    leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-    legend_1L_chr <- tmp$grobs[[leg]]
-    return(legend_1L_chr)
-}
