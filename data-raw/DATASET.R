852a36a2269a97f22ea12f60034e46d7ef1b2c92
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-07-03 08:06:33 +1000

    rebuit v

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -132,5 +132,6 @@ ready4fun::write_and_doc_fn_fls(fns_dmt_tb,
 ready4fun::write_links_for_website(user_manual_url_1L_chr = "https://github.com/ready4-dev/youthu/releases/download/v0.0.0.9065/youthu_user_0.0.0.9065.pdf",
                                    developer_manual_url_1L_chr = "https://github.com/ready4-dev/youthu/releases/download/v0.0.0.9065/youthu_developer_0.0.0.9065.pdf",
                                    project_website_url_1L_chr = "https://www.ready4-dev.com/")
+#
 
 
